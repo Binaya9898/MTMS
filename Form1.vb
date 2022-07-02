ea9531a319df62803532d9bcf8b071ff133b2828
unknown <beeenaya98@gmail.com>
2022-07-02 11:58:36 +0545

    MTMS

---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub IconButton1_Click(sender As Object, e As EventArgs) Handles SignIn.Click
+
+    End Sub
+
+    Private Sub IconButton2_Click(sender As Object, e As EventArgs) Handles About.Click
+
+    End Sub
+
+    Private Sub Contact_Click(sender As Object, e As EventArgs) Handles Contact.Click
+
+    End Sub
+
+    Private Sub IconPictureBox1_Click(sender As Object, e As EventArgs) Handles IconPictureBox1.Click
+
+    End Sub
+
+    Private Sub IconPictureBox2_Click(sender As Object, e As EventArgs) Handles IconPictureBox2.Click
+
+    End Sub
+
+    Private Sub IconPictureBox3_Click(sender As Object, e As EventArgs) Handles IconPictureBox3.Click
+
+    End Sub
+End Class
